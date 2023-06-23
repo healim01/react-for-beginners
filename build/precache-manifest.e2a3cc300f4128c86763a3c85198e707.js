91df2d1e0c55e01d9dd83375bab998ddb4c6e5f7
healim01 <22000770@handong.edu>
2023-06-23 14:06:56 +0900

    build

---
+++ build/precache-manifest.e2a3cc300f4128c86763a3c85198e707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "251dc129d114c4d45548",
+    "url": "/react-for-beginners/static/css/main.498b7e21.chunk.css"
+  },
+  {
+    "revision": "251dc129d114c4d45548",
+    "url": "/react-for-beginners/static/js/main.93164b20.chunk.js"
+  },
+  {
+    "revision": "6873a68efa9d37e96a89",
+    "url": "/react-for-beginners/static/js/runtime~main.08ccc910.js"
+  },
+  {
+    "revision": "01985d61be653f44f5da",
+    "url": "/react-for-beginners/static/js/2.53f77c8d.chunk.js"
+  },
+  {
+    "revision": "dcb5aedd4d0fe51f18f4ce6c0f01f2c6",
+    "url": "/react-for-beginners/index.html"
+  }
+];
