d7c1aa5a5514e3b9025fe4fe9f249ed657f156f4
healim01 <22000770@handong.edu>
2023-06-26 23:17:38 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-for-beginners/precache-manifest.b7951c38d36dafa78b247ba9714ac0b7.js"
+  "/react-for-beginners/precache-manifest.917c7c91b5ca6fddca1960011e4b09c9.js"
 );
 
 workbox.clientsClaim();
