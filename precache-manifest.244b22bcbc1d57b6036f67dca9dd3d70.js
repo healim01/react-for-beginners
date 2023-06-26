53553fbc177f1d9a5b37af029e6a1803d7ed7392
healim01 <22000770@handong.edu>
2023-06-27 00:38:31 +0900

    Updates

---
+++ precache-manifest.244b22bcbc1d57b6036f67dca9dd3d70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e848f0c5c9af991db40c",
+    "url": "/react-for-beginners/static/css/main.b69922cd.chunk.css"
+  },
+  {
+    "revision": "e848f0c5c9af991db40c",
+    "url": "/react-for-beginners/static/js/main.6be3b6c6.chunk.js"
+  },
+  {
+    "revision": "6873a68efa9d37e96a89",
+    "url": "/react-for-beginners/static/js/runtime~main.08ccc910.js"
+  },
+  {
+    "revision": "51b76c94ef9af090e683",
+    "url": "/react-for-beginners/static/js/2.a5abe76f.chunk.js"
+  },
+  {
+    "revision": "b6231167b1700c1cb0be34cee71ca999",
+    "url": "/react-for-beginners/index.html"
+  }
+];
